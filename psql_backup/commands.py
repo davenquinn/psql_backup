02b84ce5fa930995379b5be8e7530d0fadeab8b1
Daven Quinn <dev@davenquinn.com>
2015-03-07 12:17:54 -0800

    Using a class structure to generalize

--- psql_backup/commands.py
+++ psql_backup/commands.py
@@ -7,60 +7,65 @@ from pathlib import Path
 
 from .util import message, run
 
-def backup(DB_NAME, BACKUP_DIR):
-    """ Back up database to a PostgreSQL dump file."""
+class PSQL_Backup(object):
+    def __init__(self, db_name, backup_dir):
+        self.DB_NAME = db_name
+        self.BACKUP_DIR = Path(backup_dir)
 
-    message("Backing up database...")
+    def backup(self):
+        """ Back up database to a PostgreSQL dump file."""
 
-    fn = now().format('YYYY-MM-DD_HH.mm.ss')+".sql"
-    path = Path(BACKUP_DIR)
-    try:
-        path.mkdir()
-    except OSError:
-        pass
-    run("pg_dump -Fc {0} > {1}".format(DB_NAME, path/fn), shell=True)
-    message(style("Success!", "green"))
+        message("Backing up database...")
 
-def restore(DB_NAME, BACKUP_DIR):
-    """ Restore database from a backup."""
-
-    echo("Which backup do you want to restore?\n")
-    path = Path(BACKUP_DIR)
-    backups = list(path.glob("*.sql"))
-    for i,back in enumerate(backups[::-1]):
-        stem = back.stem
-        date = get(stem)
-        st = "  ("+style(str(i),
-            fg="green",
-            bold=True)+") "+back.stem
-        st += " - "+style(date.humanize(), fg="green")
-        echo(st)
-    echo("")
-
-    def validate_number(value):
+        fn = now().format('YYYY-MM-DD_HH.mm.ss')+".sql"
+        path = self.BACKUP_DIR
         try:
-            return backups[int(value)]
-            return backup
-        except IndexError:
-            raise click.BadParameter("Pick from the available options")
-        except ValueError:
-            raise click.BadParameter("Enter a number")
-    try:
-        backup = click.prompt("Enter a number to continue",
-                value_proc=validate_number, type=int)
-        click.echo("")
-        click.confirm("Are you sure you want to restore the database?\n"
-                      "All newer changes will be lost!", abort=True)
-    except click.exceptions.Abort:
+            path.mkdir()
+        except OSError:
+            pass
+        run("pg_dump -Fc {0} > {1}".format(self.DB_NAME, path/fn), shell=True)
+        message(style("Success!", "green"))
+
+    def restore(self):
+        """ Restore database from a backup."""
+
+        echo("Which backup do you want to restore?\n")
+        path = self.BACKUP_DIR
+        backups = list(path.glob("*.sql"))
+        for i,back in enumerate(backups[::-1]):
+            stem = back.stem
+            date = get(stem)
+            st = "  ("+style(str(i),
+                fg="green",
+                bold=True)+") "+back.stem
+            st += " - "+style(date.humanize(), fg="green")
+            echo(st)
         echo("")
-        echo("Aborting backup.")
-        return
 
-    cmd = ["pg_restore",
-            "--clean",
-            "-d "+DB_NAME,
-            str(backup)]
+        def validate_number(value):
+            try:
+                return backups[int(value)]
+                return backup
+            except IndexError:
+                raise click.BadParameter("Pick from the available options")
+            except ValueError:
+                raise click.BadParameter("Enter a number")
+        try:
+            backup = click.prompt("Enter a number to continue",
+                    value_proc=validate_number, type=int)
+            click.echo("")
+            click.confirm("Are you sure you want to restore the database?\n"
+                          "All newer changes will be lost!", abort=True)
+        except click.exceptions.Abort:
+            echo("")
+            echo("Aborting backup.")
+            return
 
-    run(*cmd)
-    message(style("Success!", "green"))
+        cmd = ["pg_restore",
+                "--clean",
+                "-d "+self.DB_NAME,
+                str(backup)]
+
+        run(*cmd)
+        message(style("Success!", "green"))
 
